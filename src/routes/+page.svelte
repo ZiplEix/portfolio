5a0b3fa7077f9c4d95f5b3d7fae40f2fcf6ab1f6
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2023-11-24 17:44:58 +0000

    Add div container and dev site title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,26 @@
+<script>
+
+</script>
 
 <main>
-    <h1>Welcome on Baptiste Leroyer's portfolio</h1>
-    <p>Soon on this page there will be all my infos and projects ;)</p>
+    <div>
+        <h1>Welcome on Baptiste Leroyer's portfolio</h1>
+        <p>Soon on this page there will be all my infos and projects ;)</p>
+    </div>
+    <div>
+        <h1>DEV SITE</h1>
+    </div>
 </main>
 
 <style>
     main {
         height: 100%;
         overflow-y: auto;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
     }
     main::-webkit-scrollbar {
         display: none;
