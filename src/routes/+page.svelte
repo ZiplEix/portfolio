e55c484c02ff5b300f3a2d9181ca9ef50536f0aa
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2023-11-24 17:31:57 +0000

    Update page layout and remove scrollbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome on Baptiste Leroyer's portfolio</h1>
-<p>Soon on this page there will be all my infos and projects ;)</p>
+
+<main>
+    <h1>Welcome on Baptiste Leroyer's portfolio</h1>
+    <p>Soon on this page there will be all my infos and projects ;)</p>
+</main>
+
+<style>
+    main {
+        height: 100%;
+        overflow-y: auto;
+    }
+    main::-webkit-scrollbar {
+        display: none;
+    }
+</style>
