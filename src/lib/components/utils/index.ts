a62d1dbcfa9d40c4b89692c7855added8fc16ac1
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2023-11-28 22:56:21 +0000

    Add column and row utility components

---
+++ src/lib/components/utils/index.ts
@@ -0,0 +1,4 @@
+import Row from "./row.svelte";
+import Column from "./column.svelte";
+
+export { Row, Column };
---
+++ src/lib/components/utils/row.svelte
@@ -0,0 +1,37 @@
+<script lang='ts'>
+    export let alignment: 'start' | 'center' | 'end' | 'spaceAround' | 'spaceEvenly' = 'center';
+</script>
+
+<!-- a svelte component to do somethink similar to the Row widget in flutter, the flex depend on the alignment property -->
+
+<style>
+    .row {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .start {
+        align-items: flex-start;
+    }
+
+    .center {
+        align-items: center;
+    }
+
+    .end {
+        align-items: flex-end;
+    }
+
+    .spaceAround {
+        align-items: space-around;
+    }
+
+    .spaceEvenly {
+        align-items: space-evenly;
+    }
+</style>
+
+<div class="row {alignment}">
+    <slot />
+</div>
