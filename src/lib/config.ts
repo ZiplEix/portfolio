9f85ff46b84d46d83719ac276bababb669933b85
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2023-12-19 21:09:42 +0100

    Update title and description in config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from "$app/environment"
 
-export const title = "markdown test blog"
-export const description = "sveltkit blog for poets"
+export const title = "Baptiste's portfolio"
+export const description = "The portfolio of Baptiste Leroyer made in SVeltekit."
 export const url = dev ? "http://localhost:5173" : "https://baptiste.zip"
