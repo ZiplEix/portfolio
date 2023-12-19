053f05eb340f0e10158d5392f4b052bccc856381
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2023-12-19 21:10:34 +0100

    Add footer, header, toggle, and transition components

---
+++ src/routes/header.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import * as config from "$lib/config"
+  import Toggle from "./toggle.svelte";
+</script>
+
+<nav>
+    <a href="/" class="title">
+        <b>{config.title}</b>
+    </a>
+
+    <Toggle />
+</nav>
+
+
+<style>
+    nav {
+        padding-block: var(--size-7);
+        align-items: center;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+
+        font-size: var(--font-size-fluid-1);
+    }
+
+    @media (min-width: 768px) {
+        nav {
+            display: flex;
+            justify-content: space-between;
+        }
+    }
+</style>
